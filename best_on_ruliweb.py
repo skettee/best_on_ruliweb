220856e8d49df1405d778dcfd47dc54f49354e95
skettee <dohyeong.lee@gmail.com>
2019-04-18 09:31:19 +0900

    code refactoring

--- best_on_ruliweb.py
+++ best_on_ruliweb.py
@@ -1,3 +1,7 @@
+#
+# 오른쪽 Best on 루리웹
+#
+#%%
 import requests
 from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
@@ -6,14 +10,17 @@ import json
 from datetime import datetime
 from datetime import timedelta
 
-# moabot 컴포넌트를 가져온다.
-import moabot_database as moabot
-from moabot_id import *
+if __debug__:
+    import os.path
+
+# 모아보기 컴포넌트를 가져온다.
+import moabogey_database as moabogey
+from moabogey_id import *
 
 # 사이트 이름
 site_name = 'ruliweb'
-# 사이트에서 가져올 내용
-section_name = 'best'
+# 사이트에서 가져올 주제
+subject_name = 'best'
 # 사이트 주소
 site_url = 'https://bbs.ruliweb.com/best/hit_history'
 print('site url is: ', site_url)
@@ -35,20 +42,18 @@ else:
     # BeautifulSoup 오브젝트를 생성한다.
     soup = BeautifulSoup(html_source, 'html.parser')
     
-    # 디버그를 위해서 page의 소스를 파일로 저장한다.
-    # 봇으로 등록하기 위해서는 주석처리를 해야한다.
-    #import os.path
-    #file_name = site_name + '_requests.html'
-    #if not os.path.isfile(file_name):
-    #    print('file save: ', file_name)
-    #    with open(file_name, 'w') as f:
-    #        f.write(soup.prettify())
+    # HTML을 분석하기 위해서 페이지의 소스를 파일로 저장한다.
+    if __debug__:
+        file_name = site_name + '_source.html'
+        if not os.path.isfile(file_name):
+            print('file save: ', file_name)
+            with open(file_name, 'w', encoding='utf-8') as f:
+                f.write(soup.prettify())
 
-    # 데이터를 저장할 데이터베이스를 연다. 
-    # bot_id는 moabot_id에서 가져오는 변수값이다.
-    # 프로그램을 종료하기 전에 데이터 베이스를 닫아야 한다.
-    db_name = section_name + '_on_' + site_name 
-    my_db = moabot.Dbase(db_name, bot_id)
+    # 데이터를 저장할 데이터베이스를 생성한다. 
+    # bot_id는 moabogey_id에서 가져온 값이다.
+    db_name = subject_name + '_on_' + site_name 
+    my_db = moabogey.Dbase(db_name, bot_id)
     
     # 반복해서 리스트의 목록을 하나씩 검색하고 데이터를 수집한다.
     for post in soup.find_all('div', class_='article_info'):
@@ -68,19 +73,23 @@ else:
                 print(f'Other error occurred: {err}')
         else:
             subhtml_source = response.text
-            subsoap = BeautifulSoup(subhtml_source, 'html.parser')
-            #with open(f'{site_name}_section_request.html', 'w') as f:
-            #    f.write(subsoap.prettify())
-            #break
+            post = BeautifulSoup(subhtml_source, 'html.parser')
+            # HTML을 분석하기 위해서 포스트의 소스를 파일로 저장한다.
+            if __debug__:
+                file_name = site_name + '_post_source.html'
+                if not os.path.isfile(file_name):
+                    print('file save: ', file_name)
+                    with open(file_name, 'w', encoding='utf-8') as f:
+                        f.write(post.prettify())
             
             # 사이트 이름을 수집한다.
-            moa_site_name = subsoap.find('meta', property="og:site_name")
+            moa_site_name = post.find('meta', property="og:site_name")
             if moa_site_name:
                 moa_site_name = moa_site_name['content']
             #print('siteName: ', moa_site_name)
             
             # 포스트의 대표 이미지의 주소를 수집한다.
-            moa_image = subsoap.find('meta', property="og:image")
+            moa_image = post.find('meta', property="og:image")
             if moa_image:
                 moa_image = moa_image['content']
                 # thumb 이미지를 일반 이미지로 변경
@@ -88,15 +97,15 @@ else:
             #print('image: ', moa_image)
             
             # 포스트 제목을 수집/가공 한다.
-            moa_title = subsoap.find('span', class_='subject_text').text.strip()
+            moa_title = post.find('span', class_='subject_text').text.strip()
             #print('title: ', moa_title)            
             
             # 포스트를 올린 사람의 ID를 수집한다.
-            moa_createdBy = subsoap.find('strong', class_='nick').text.strip()
+            moa_createdBy = post.find('strong', class_='nick').text.strip()
             #print('createdBy: ', moa_createdBy)
 
             # 포스트를 올린 날짜를 수집한다.
-            post_time = subsoap.find('span', class_='regdate').text.strip()
+            post_time = post.find('span', class_='regdate').text.strip()
             # 2019.03.21 (01:53:19)
             moa_createdAt = datetime.strptime(post_time, '%Y.%m.%d (%H:%M:%S)')
             # UTC 값으로 변경하기 위해서 9시간을 뺀다. 
@@ -113,7 +122,7 @@ else:
         
             item_text = ''
             # 포스트 요약을 수집/가공한다.
-            for item in subsoap.find('div', class_='view_content').find_all('p'):
+            for item in post.find('div', class_='view_content').find_all('p'):
                 #print(item.text)
                 a = item.text.strip()
                 if(a != ''):
@@ -145,17 +154,23 @@ else:
                     'timeStamp': moa_timeStamp
                 }
 
-                # 디버그를 위해서 수집한 데이터를 출력한다.
-                temp_data = db_data.copy()
-                temp_data['desc'] = temp_data['desc'][:50]
-                print(json.dumps(temp_data, indent=4, ensure_ascii=False, default=str))
+                if __debug__:
+                    # 디버그를 위해서 수집한 데이터를 출력한다.
+                    temp_data = db_data.copy()
+                    temp_data['desc'] = temp_data['desc'][:20] + '...'
+                    print('collected json data: ')
+                    print(json.dumps(temp_data, indent=4, ensure_ascii=False, default=str))
 
                 # 수집한 데이터를 데이터베이스에 전송한다.
                 my_db.insertTable(db_data) 
                 
-                # one time break
+                # 수집이 완료되면 루프를 종료한다.
                 break
                 
+    # 데이터 베이스에 저장된 데이터를 디스플레이 한다.
+    if __debug__:
+        my_db.displayHTML()
+
     # 데이터 베이스를 닫는다.
     my_db.close()
     
